546ad406c1d748ba16e8f0354ef5836ebfc7413d
benkrikler <mr.krikler@googlemail.com>
2017-03-09 16:43:07 +0000

    Implement merge functionality (#1)
    * Ignore vim swap files
    
    * Implement the merge command
    
    * Remove debug stuff
    
    * Switch to accepting multiple filenames and being able to combine them to
    one output

--- lz_cli/commands/merge/__init__.py
+++ lz_cli/commands/merge/__init__.py
@@ -1,4 +1,43 @@
+"""
+    merge:
+        Add an input CSV file into a summary output file
+
+    Usage:
+        merge [options] <input_file>[ <input_file2>[...]] <summary_file>
+
+    Options:
+        --append       Instead of overwriting the summary file, append the input data to it
+"""
 import hepshell
+import pandas as pd
 
 class Command(hepshell.Command):
-    pass
+    DEFAULTS = dict(
+    )
+
+    def __init__(self, path=__file__, doc=__doc__):
+        doc = doc.format(**Command.DEFAULTS)
+        super(Command, self).__init__(path, doc)
+
+    def run(self, args, variables):
+        self.__prepare(args,variables)
+        if len(self.__args)<2:
+            self.__text="Error: merge command needs at least 2 arguments"
+            return False
+
+        input_filenames=args[:-1]
+        summary_filename=args[-1]
+
+        if self.__variables.get("append",False) is True:
+            input_filenames=[summary_filename]+input_filenames
+
+        summary=None
+        for input_file in input_filenames:
+            to_add=pd.read_csv(input_file)
+            if summary is None:
+                summary=to_add
+            else:
+                summary=summary.append(to_add,ignore_index=True)
+
+        summary.to_csv(summary_filename,index=False)
+        return True
